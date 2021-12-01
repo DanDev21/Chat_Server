0647ff38e7862f701c5f66b3fc2d15dc1b41962d
Dan Sabau <danioansabau@gmail.com>
2021-12-01 11:45:54 +0200

    gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
-val ktor_version: String by project
-val kotlin_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val kmongoVersion: String by project
+val koinVersion: String by project
 
 plugins {
     application
@@ -10,7 +12,7 @@ plugins {
 val group = "com.example"
 val version = "0.0.1"
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    this.mainClass.set("io.ktor.server.netty.EngineMain")
 }
 
 repositories {
@@ -18,12 +20,31 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-server-sessions:$ktor_version")
-    implementation("io.ktor:ktor-serialization:$ktor_version")
-    implementation("io.ktor:ktor-websockets:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
+
+    // default plugins
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-sessions:$ktorVersion")
+    implementation("io.ktor:ktor-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-websockets:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:1.2.7")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+
+    // mongo db
+    implementation("org.litote.kmongo:kmongo:$kmongoVersion")
+    implementation("org.litote.kmongo:kmongo-coroutine:$kmongoVersion")
+
+
+    // koin
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("io.insert-koin:koin-ktor:$koinVersion")
+    implementation("io.insert-koin:koin-logger-slf4j:$koinVersion")
+
+
+    // testing - ktor
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+
+
+    // testing - junit
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
