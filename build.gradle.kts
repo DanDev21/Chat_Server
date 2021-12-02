79af0cd931b6fff36f8dd1ee4e0929f0a495d4d2
Dan Sabau <danioansabau@gmail.com>
2021-12-02 09:37:14 +0200

    gradle setup for deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 val ktorVersion: String by project
 val kotlinVersion: String by project
 val kmongoVersion: String by project
@@ -7,17 +9,22 @@ plugins {
     application
     kotlin("jvm") version "1.6.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.0"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 val group = "com.example"
 val version = "0.0.1"
 application {
     this.mainClass.set("io.ktor.server.netty.EngineMain")
+    project.setProperty("mainClassName", mainClass.get())
 }
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
+
+val sshAntTask = configurations.create("sshAntTask")
 
 dependencies {
 
@@ -28,6 +35,10 @@ dependencies {
     implementation("io.ktor:ktor-websockets:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:1.2.7")
+
+
+    //
+    sshAntTask("org.apache.ant:ant-jsch:1.9.2")
 
 
     // mongo db
@@ -47,4 +58,74 @@ dependencies {
 
     // testing - junit
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
+
+tasks.withType<ShadowJar> {
+    manifest {
+        attributes(
+            "Main-Class" to application.mainClass.get()
+        )
+    }
+}
+
+ant.withGroovyBuilder {
+    "taskdef"(
+        "name" to "scp",
+        "classname" to "org.apache.tools.ant.taskdefs.optional.ssh.Scp",
+        "classpath" to configurations.get("sshAntTask").asPath
+    )
+    "taskdef"(
+        "name" to "ssh",
+        "classname" to "org.apache.tools.ant.taskdefs.optional.ssh.SSHExec",
+        "classpath" to configurations.get("sshAntTask").asPath
+    )
+}
+
+
+task("deploy") {
+    dependsOn("clean", "shadowJar")
+    ant.withGroovyBuilder {
+        doLast {
+            val knownHosts = File.createTempFile("knownhosts", "txt")
+            val user = "root"
+            val host = "34.79.179.182"
+            val pk = file("keys/ktorchatapp")
+            val jarFileName = "QChat-all.jar"
+            try {
+                "scp"(
+                    "file" to file("build/libs/$jarFileName"),
+                    "todir" to "$user@$host:/root/chat",
+                    "keyfile" to pk,
+                    "trust" to true,
+                    "knownhosts" to knownHosts
+                )
+                "ssh"(
+                    "host" to host,
+                    "username" to user,
+                    "keyfile" to pk,
+                    "trust" to true,
+                    "knownhosts" to knownHosts,
+                    "command" to "mv /root/chat/$jarFileName /root/chat/chat-server.jar"
+                )
+                "ssh"(
+                    "host" to host,
+                    "username" to user,
+                    "keyfile" to pk,
+                    "trust" to true,
+                    "knownhosts" to knownHosts,
+                    "command" to "systemctl stop chat"
+                )
+                "ssh"(
+                    "host" to host,
+                    "username" to user,
+                    "keyfile" to pk,
+                    "trust" to true,
+                    "knownhosts" to knownHosts,
+                    "command" to "systemctl start chat"
+                )
+            } finally {
+                knownHosts.delete()
+            }
+        }
+    }
 }
